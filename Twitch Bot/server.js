607597dfac157f7b7962f4bd533e759c584f0dd5
oxygengamestudio <lucasdamour@icloud.com>
2022-07-21 23:56:56 +0200

    V 0.2

--- Twitch Bot/server.js
+++ Twitch Bot/server.js
@@ -2,44 +2,29 @@ require('dotenv').config();
 
 const tmi = require('tmi.js');
 
-const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/);
-
-const commands = {
-    website: {
-      response: 'https://spacejelly.dev'
-    },
-    upvote: {
-      response: (argument) => `Successfully upvoted ${argument}`
-    }
-  }
-
 const client = new tmi.Client({
-    connection:{
-        reconnect: true
-    },
-	channels: [ 'cooxybot' ],
-    identity: {
-		username: process.env.TWITCH_BOT_USERNAME,
-		password: process.env.TWITCH_OAUTH_TOKEN
-    }
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true
+  },
+  identity: {
+    username: process.env.TWITCH_BOT_USERNAME,
+    password: process.env.TWITCH_OAUTH_TOKEN
+  },
+  channels: ['mastersnakou']
 });
 
-client.connect();
+let loopInterval
+  client.on('chat', (channel, userstate, message, self) => {
+    console.log(`Message "${message}" received from ${userstate['display-name']}`)
+    if (self) return
+    const msg = message.split(' ')
+    
+    console.log('start $loop')
+    loopInterval = setInterval(function () {
+      client.say(channel, 'masterLove masterLove masterLove masterLove masterLove masterLove') // client.say(channel, msg[1]) // ?
+    }, 36000000) // 60000ms = 60s = 1min
+  })
 
-client.on('message', async (channel, context, message) => {
-    const isNotBot = context.username.toLowerCase() !== process.env.TWITCH_BOT_USERNAME.toLowerCase();
-  
-    if ( !isNotBot ) return;
-
-	if ( !commands) return;
-  
-    const [raw, command, argument] = message.match(regexpCommand);
-  
-    const { response } = commands[command] || {};
-  
-    if ( typeof response === 'function' ) {
-      client.say(channel, response(argument));
-    } else if ( typeof response === 'string' ) {
-      client.say(channel, response);
-    }
-  });
+  client.connect();
